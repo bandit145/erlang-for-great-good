985b429afed051da60632f833b471a3ef9a2ed68
Philip Bove <phil@bove.online>
2020-07-10 17:04:18 -0400

    did chapter17

---
+++ chapter16/curling_scoreboard_hw.erl
@@ -0,0 +1,16 @@
+-module(curling_scoreboard_hw).
+-export([add_point/1, next_round/0, set_teams/2, reset_board/0]).
+
+%% this is a dumb module that only there to replace relase hw
+
+set_teams(TeamA, TeamB) ->
+	io:format("Scoreboard: Team ~s vs. Teams ~s~n", [TeamA, TeamB]).
+
+next_round() ->
+	io:format("Scoreboard: round over~n").
+
+add_point(Team) ->
+	io:format("Scoreboard: increased score of team ~s by 1~n", [Team]).
+
+reset_board() ->
+	io:format("Scoreboard: All teams are undefined and all scores are 0~n").
